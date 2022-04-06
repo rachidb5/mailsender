27b9d57efcd897e73ec13720ce865c69071e3306
robbastos <roberto.bastos@prolins.com.br>
2022-04-06 15:27:59 -0300

    pro pedro ver

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const corsOpts = {
-    origin: '*',
-  
-    methods: [
-      'GET',
-      'POST',
-      'PUT'
-    ],
-  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
-  
-  app.use(cors(corsOpts));
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+    'PUT'
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts));
 app.use(routes);
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log('Acessar http://localhost:3001');
+  console.log('Acessar http://localhost:3001');
 });
--- mailSender.js
+++ mailSender.js
@@ -1,25 +1,26 @@
 const nodemailer = require("nodemailer");
 require('dotenv').config();
+const { base64encode, base64decode } = require('nodejs-base64');
 
-exports.mailSender = async(req, res) =>{
-    try {    
-      // create reusable transporter object using the default SMTP transport
-      let transporter = nodemailer.createTransport({
-        host: "mail.cremers.org.br",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-          user: 'naoresponda@cremers.org.br', // generated ethereal user
-          pass: '', // generated ethereal password
-        },
-      });
-    
-      // send mail with defined transport object
-      let info = await transporter.sendMail({
-        from: '"Cremers" <naoresponda@cremers.org.br>', // sender address
-        to: "jordan.rachid@gmail.com", // list of receivers
-        subject: "Novo contato via Site (Denúncia)", // Subject line
-        text: `
+exports.mailSender = async (req, res) => {
+  try {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "mail.cremers.org.br",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'naoresponda@cremers.org.br', // generated ethereal user
+        pass: process.env.PASSWORD, // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Cremers" <naoresponda@cremers.org.br>', // sender address
+      to: "jordan.rachid@gmail.com", // list of receivers
+      subject: "Novo contato via Site (Denúncia)", // Subject line
+      text: `
         CRM: ${req.body.cremers}
         nome: ${req.body.nome}
         CPF: ${req.body.cpf}
@@ -29,27 +30,34 @@ exports.mailSender = async(req, res) =>{
         instituicao: ${req.body.instituicao}
         cidade:${req.body.cidade}
               `, // plain text body
-        //html: "<b>Hello world?</b>", // html body
-      });
-    
-      console.log("Message sent: %s", info.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-      return res.status(201).send(req.body)
-        } catch (e) {
-                console.log(e);
-                return res.status(400).json({ error: e });
-            }
+      //html: "<b>Hello world?</b>", // html body
+      attachments: [
+        { 
+          filename: 'relato.pdf',
+          content: base64decode(req.body.relato)
+        },
+
+      ]
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    return res.status(201).send(req.body)
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ error: e });
+  }
 };
 
-exports.tester = async(req, res) =>{
+exports.tester = async (req, res) => {
   try {
     return res.status(200).send('ts')
   } catch (e) {
     console.log(e);
     return res.status(400).json({ error: e });
-}
+  }
 }
